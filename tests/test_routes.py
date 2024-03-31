afd2e02f63fc08f67a9ea2e246e4899bf211c409
norbeyalzate <norbeyalzate163437@correo.itm.edu.co>
2024-03-31 09:13:13 -0400

    fixing pipeline

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -137,12 +137,10 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
 
-
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
-
     
     def test_update_account(self):
         """It should Update an existing Account"""
@@ -198,3 +196,4 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check for the CORS header
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
+        
